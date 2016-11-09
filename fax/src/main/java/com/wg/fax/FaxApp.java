163b69228f633b06a0d0834f0e3e67118c153bf9
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2016-11-09 21:14:28 +0300

    Added producers

---
+++ fax/src/main/java/com/wg/fax/FaxApp.java
@@ -0,0 +1,12 @@
+package com.wg.fax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaxApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaxApp.class, args);
+    }
+}
---
+++ web-interface/src/main/java/com/wg/web/WebInterfaceApp.java
@@ -0,0 +1,12 @@
+package com.wg.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInterfaceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebInterfaceApp.class, args);
+    }
+}
